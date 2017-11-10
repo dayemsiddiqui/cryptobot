4786293194fc975b64e2f644e85fb03dd85076ee
frosty <desipher.ams@gmail.com>
2017-11-10 13:25:57 +0500

    Basic Concept

--- woxcut_web/src/components/Dashboard/Views/Dashboard/StrategyList.vue
+++ woxcut_web/src/components/Dashboard/Views/Dashboard/StrategyList.vue
@@ -1,103 +1,109 @@
 <template>
 
-  <div class="card">
+  <el-row :gutter="40">
 
-    <div class="card-header">
-      <h4 class="card-title">Strategy List</h4>
-      <p class="category">List of Strategies</p>
-    </div>
+    <el-col :span="12">
 
-    <div class="card-content row">
+      <el-table 
+        class="table table-no-bordered table-hover"
+        :data="strategyList"
+        highlight-current-row
+        @current-change="handleCurrentChange">
 
-      <div class="col-sm-12">
+        <el-table-column type="index">
+        </el-table-column>
+        
+        <el-table-column prop="name"
+                         label="Strategy">
+        </el-table-column>
 
-        <el-table class="table table-no-bordered table-hover"
-                  :data="strategies">
+        <el-table-column prop="type"
+                         label="Type">
+        </el-table-column>
 
-          <el-table-column type="index">
-          </el-table-column>
-          
-          <el-table-column prop="name"
-                           label="Strategy">
-          </el-table-column>
+      </el-table>
 
-          <el-table-column prop="type"
-                           label="Type">
-          </el-table-column>
+    </el-col> <!-- end el-col -->
 
-          <el-table-column
-            label="Actions">
-            <template scope="props">
-              <a class="btn btn-simple btn-info btn-xs btn-icon like" @click="handleLike(props.$index, props.row)"><i class="ti-heart"></i></a>
-              <a class="btn btn-simple btn-warning btn-xs btn-icon edit" @click="handleEdit(props.$index, props.row)"><i class="ti-pencil-alt"></i></a>
-              <a class="btn btn-simple btn-danger btn-xs btn-icon remove"  @click="handleDelete(props.$index, props.row)"><i class="ti-close"></i></a>
-            </template>
-          </el-table-column>
 
-        </el-table>
+    <el-col :span="12">
 
-      </div> <!-- end col-sm-12 -->
-    </div> <!-- end card-content row -->
+      <div v-bind:class="isSelected ? 'card' : ''">
+       <el-row>
+        <div class="card-header text-center">
+          <h1 class="card-title">
+            {{ selectedStrategy.name }}
+          </h1>
+        </div>
 
-  </div> <!-- end card -->
+        <div class="card-content text-center">
+          {{ selectedStrategy.type }}
+        </div>
+
+       </el-row>
+      </div>  <!-- end card div -->
+
+    </el-col> <!-- end el-col -->
+
+  </el-row> <!-- end el-row -->
 
 </template>
 
 <script>
 
-export default {
-  components: {
-  },
-  data () {
-    return {
-      strategies: [
-      {
-        name: CCI,
-        type: Free
-      },
-      {
-        name: DEMA,
-        type: Free
-      },
-      {
-        name: MACD,
-        type: Free
-      },
-      {
-        name: PPO,
-        type: Free
-      },
-      {
-        name: RSI,
-        type: Free
-      },
-      {
-        name: StochRSI,
-        type: Free
-      },
-      {
-        name: TSI,
-        type: Free
-      },
-      {
-        name: UO,
-        type: Free
-      },
-    ]
-  }
-},
-  methods: {
-    handleLike (index, row) {
-      alert(`Your clicked on Like button`)
+  import Vue from 'vue'
+  import 'element-ui/lib/theme-default/index.css'
+  import {Row, Col, Table, TableColumn} from 'element-ui'
+  Vue.use(Row)
+  Vue.use(Col)
+  Vue.use(Table)
+  Vue.use(TableColumn)
+
+
+  export default {
+    data () {
+      return {
+        isSelected: false,
+        selectedStrategy: {
+          name: '',
+          type: ''
+        },
+        strategyList: [{
+          name: 'CCI',
+          type: 'Free'
+        }, {
+          name: 'DEMA',
+          type: 'Free'
+        }, {
+          name: 'MACD',
+          type: 'Free'
+        }, {
+          name: 'PPO',
+          type: 'Free'
+        }, {
+          name: 'RSI',
+          type: 'Free'
+        }, {
+          name: 'StochRSI',
+          type: 'Free'
+        }, {
+          name: 'TSI',
+          type: 'Free'
+        }, {
+          name: 'UO',
+          type: 'Free'
+        }]
+      }
     },
-    handleEdit (index, row) {
-      alert(`Your want to edit ${row.name}`)
-    },
-    handleDelete (index, row) {
-      alert(`Your want to delete ${row.name}`)
+
+    methods: {
+      handleCurrentChange(val) {
+        val ? this.isSelected = true : this.isSelected = false;
+        this.selectedStrategy = val;
+        console.log(this.selectedStrategy);
+      }
     }
   }
-}
 
 </script>
 
