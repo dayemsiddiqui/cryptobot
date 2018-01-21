35e7511a85ac12abeeb66da8dd956999379ad8bf
spokzers <spokzers@gmail.com>
2018-01-21 19:33:57 +0500

    Fixed Linting issues

--- backend/src/exchanges/binance/index.js
+++ backend/src/exchanges/binance/index.js
@@ -10,12 +10,12 @@ binance.options({
 /* let getDate = unixTimestamp => {
 // unixTimestamp in miliseconds
   let date = moment(unixTimestamp)
-  let day =	date.format('DD')
-  let month =	date.format('MM')
-  let year =	date.format('YYYY')
-  let hours =	date.format('HH')
-  let minutes	=	date.format('mm')
-  let seconds	=	date.format('ss')
+  let day = date.format('DD')
+  let month = date.format('MM')
+  let year =  date.format('YYYY')
+  let hours = date.format('HH')
+  let minutes = date.format('mm')
+  let seconds = date.format('ss')
   return [date, day, month, year, hours, minutes, seconds]
 } */
 
@@ -31,14 +31,14 @@ export const get5minuteCandleSticks = (startTime, endTime, symbol, callback) => 
 
   for (let i = 1; i <= noOfRequiredRequests; i++) {
     binance.candlesticks(symbol, '5m', (error, ticks, symbol) => {
-		/* FORMAT */
-		// time, open, high, low, close, volume, closeTime, assetVolume, trades, buyBaseVolume, buyAssetVolume, ignored
+    /* FORMAT */
+    // time, open, high, low, close, volume, closeTime, assetVolume, trades, buyBaseVolume, buyAssetVolume, ignored
       if (!error) {
-      	candlesticks = candlesticks.concat(ticks)
-      	callbacksDone++
-      	if (callbacksDone === noOfRequiredRequests) {
-      		callback(candlesticks)
-      	}
+        candlesticks = candlesticks.concat(ticks)
+        callbacksDone++
+        if (callbacksDone === noOfRequiredRequests) {
+          callback(candlesticks)
+        }
       }
     },
       {
