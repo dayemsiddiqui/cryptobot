d6b9c84ba132a0949fcf6fe96d2d1a64c4ee2f6c
Abdul Moiz Sheikh <desipher.ams@gmail.com>
2018-01-24 02:06:23 +0500

    Refactored and wrapped a promise around callbacks

--- backend/src/exchanges/binance/index.js
+++ backend/src/exchanges/binance/index.js
@@ -1,3 +1,11 @@
+/* USAGE */
+/*
+// Doesn't handle errors
+get5minuteCandleSticks(1499990400000, 1516460405000, 'BTCUSDT').then((candlesticks) => {
+  console.log(candlesticks)
+})
+*/
+
 import binance from 'node-binance-api'
 // import moment from 'moment'
 
@@ -20,8 +28,7 @@ binance.options({
 } */
 
 // returns an array of candlesticks
-export const get5minuteCandleSticks = async (startTime, endTime, symbol, callback) => {
-// export const get5minuteCandleSticks = (startTime, endTime, symbol, callback) => {
+/* export const get5minuteCandleSticks = (startTime, endTime, symbol, callback) => {
   let timestampDifference = Math.abs(endTime - startTime)
   // 2500 minutes
   const INCREMENT = 150000000
@@ -32,7 +39,6 @@ export const get5minuteCandleSticks = async (startTime, endTime, symbol, callbac
 
   for (let i = 1; i <= noOfRequiredRequests; i++) {
     binance.candlesticks(symbol, '5m', (error, ticks, symbol) => {
-    /* FORMAT */
     // time, open, high, low, close, volume, closeTime, assetVolume, trades, buyBaseVolume, buyAssetVolume, ignored
       if (!error) {
         candlesticks = candlesticks.concat(ticks)
@@ -52,5 +58,53 @@ export const get5minuteCandleSticks = async (startTime, endTime, symbol, callbac
 
   candlesticks = candlesticks.sort((a, b) => {
     return a[0] - b[0]
+  })
+} */
+
+/*
+const promisifiedBinanceCandlesticks = (symbol, interval) => {
+  return new Promise((resolve, reject) => {
+    binance.candlesticks(symbol, interval, (error, ticks, symbol) => {
+      if (error != null) {
+        reject(error)
+      }
+      resolve(ticks)
+    })
+  })
+}
+*/
+
+// returns an array of candlesticks
+export const get5minuteCandleSticks = (startTime, endTime, symbol) => {
+  return new Promise((resolve, reject) => {
+    let timestampDifference = Math.abs(endTime - startTime)
+    // 2500 minutes
+    const INCREMENT = 150000000
+    let noOfRequiredRequests = Math.ceil(timestampDifference / INCREMENT)
+    let currentTime = startTime
+    let candlesticks = []
+    let callbacksDone = 0
+
+    for (let i = 1; i <= noOfRequiredRequests; i++) {
+      binance.candlesticks(symbol, '5m', (error, ticks, symbol) => {
+      // time, open, high, low, close, volume, closeTime, assetVolume, trades, buyBaseVolume, buyAssetVolume, ignored
+        if (!error) {
+          candlesticks = candlesticks.concat(ticks)
+          callbacksDone++
+          if (callbacksDone === noOfRequiredRequests) {
+            candlesticks = candlesticks.sort((a, b) => {
+              return a[0] - b[0]
+            })
+            resolve(candlesticks)
+          }
+        }
+      },
+        {
+          limit: 500,
+          startTime: currentTime,
+          endTime: currentTime + INCREMENT
+        })
+      currentTime = currentTime + INCREMENT
+    }
   })
 }
