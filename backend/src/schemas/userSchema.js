141a3ba8fa9fd219b4a68f62db67072774ded5d6
spokzers <spokzers@gmail.com>
2018-02-07 01:01:23 +0500

    removed husky and rewrote the precommit hook manually

--- backend/src/schemas/userSchema.js
+++ backend/src/schemas/userSchema.js
@@ -29,8 +29,8 @@ userSchema.pre('save', function (next) {
     if (err) return next(err)
     bcrypt.hash(user.password, salt, null, (err, hash) => {
       if (err) return next(err)
-user.password = hash
-      next();
+      user.password = hash
+      next()
     })
   })
 })
