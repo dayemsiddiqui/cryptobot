596a6b87dbe37180f876e1218c3f1648aade95a0
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2018-05-23 13:03:31 +0500

    50% of the work

--- frontend/src/components/Dashboard/Portfolio/PortfolioTracker.vue
+++ frontend/src/components/Dashboard/Portfolio/PortfolioTracker.vue
@@ -10,7 +10,7 @@
           <hr>
           <div class="card-content">
           
-            <column-chart :data="[['Sun', 32], ['Mon', 46], ['Tue', 28]]"></column-chart>   
+            <column-chart :data="portfolioData"></column-chart>   
           </div> <!-- end card-content -->
       </div> <!-- end card -->
     </div>
@@ -25,7 +25,7 @@
               <hr>
               <div class="card-content">
               
-                <column-chart :data="[['Sun', 32], ['Mon', 46], ['Tue', 28]]"></column-chart>   
+                <column-chart :data="portfolioData"></column-chart>   
               </div> <!-- end card-content -->
           </div> <!-- end card -->
       </div>
@@ -37,18 +37,11 @@
               </div>
               <hr>
               <div class="card-content">
-                <el-select
-                  class="select-info"
-                  size="large"
-                  placeholder="First Currency"
-                  v-model="model.currencies">
-                    <el-option
-                      v-for="item in model.currencies"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value">
-                    </el-option>
-                  </el-select>
+                <v-select label="label" v-model="selected" :options="model.currencies" placeholder="Select a currency"></v-select>
+                <br>   
+                <input type="number" v-model="amount" placeholder="Quantity" style="width: 100%;" /> 
+                <br><br>
+                <button class="btn btn-primary btn-sm" @click="add">Add Investment</button>          
                   
               </div> <!-- end card-content -->
           </div> <!-- end card -->
@@ -65,24 +58,36 @@
   import {Row, Col} from 'element-ui'
   import {VueTabs, VTab} from 'vue-nav-tabs'
   import {mapFields} from 'vee-validate'
-
+  
   Vue.use(Row)
   Vue.use(Col)
   Vue.use(VueTabs)
   Vue.use(VTab)
-
+  
+  // eslint-disable-next-line 
+  Vue.component('v-select', VueSelect.VueSelect)
   export default {
     computed: {
       ...mapFields(['buyC1', 'buyC2', 'sellC1', 'sellC2', 'c1List', 'c2List'])
     },
     created () {
-      // runs after component is created
+      // https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH,BTC,REP&tsyms=USD
     },
     data () {
       return {
+        selected: '',
+        amount: '',
         tabTitles: ['Buy', 'Sell'],
         currentTab: 'Buy',
         currentTabColor: '',
+        holdings: {},
+        portfolioData: [],
+        priceList: {
+          BTC: 254.5,
+          ETH: 256.5,
+          REP: 12,
+          LTC: 75
+        },
         model: {
 
           buyC1: '',
@@ -95,21 +100,17 @@
 
           currencies: [{
             value: 'BTC',
-            label: 'BTC'
+            label: 'Bitcoin'
           }, {
             value: 'ETH',
-            label: 'ETH'
-          }, {
-            value: 'USD',
-            label: 'USD'
+            label: 'Etheruem'
           }, {
             value: 'REP',
-            label: 'REP'
+            label: 'Ripple'
           }, {
             value: 'LTC',
-            label: 'LTC'
+            label: 'Litecoin'
           }]
-
         }
       }
     },
@@ -122,12 +123,34 @@
           this.$emit('on-submit', this.registerForm, isValid)
         })
       },
+      transform () {
+        console.log(Object.keys(this.holdings))
+        var keys = Object.keys(this.holdings)
+        var data = []
+        for (var i = 0; i < keys.length; i++) {
+          data.push([keys[i], this.holdings[keys[i]]])
+        }
+        console.log('Transformed', data)
+        this.portfolioData = data
+      },
       handleTabChange (tabIndex, newTab, oldTab) {
         // console.log(tabIndex, newTab.title, oldTab.title)
         this.currentTab = newTab.title
         if (this.currentTab === 'Buy') this.currentTabColor = '#AAE9BD'
         if (this.currentTab === 'Sell') this.currentTabColor = '#F38F72'
+      },
+      add () {
+        console.log('Added the investment', this.amount, this.selected.value)
+        if (this.holdings[this.selected.value]) {
+          this.holdings[this.selected.value] += parseInt(this.amount)
+        } else {
+          this.holdings[this.selected.value] = parseInt(this.amount)
+        }
+        this.amount = ''
+        this.selected = ''
+        this.transform()
       }
+
     }
   }
 </script>
